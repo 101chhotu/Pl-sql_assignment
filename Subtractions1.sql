0b5357f08c3402398eb876bc9f88e59b548431c3
Ravaliya_khushi <110173132+101chhotu@users.noreply.github.com>
2023-12-29 21:21:28 +0530

    Create Subtractions1.sql

---
+++ Subtractions1.sql
@@ -0,0 +1,14 @@
+set serveroutput on;
+
+Declare
+   num1 number(2);
+   num2 number(2);
+   sub1 number(4);
+Begin
+   num1:=&num1;
+   num2:=&num2;
+   sub1:=num1-num2;
+
+   dbms_output.put_line('addition of two number:'||sub1);
+End;
+/
